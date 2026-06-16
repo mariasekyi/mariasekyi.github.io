d5e1d2baaccfd14018225cff5ee2be9a7bee4e7c
mariahsekyi <maria1sekyi@gmail.com>
2026-06-16 14:24:49 -0500

    Adding home css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
+import styles from "./Home.module.css"
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <div className={styles.container}>
+      <aside className={styles.sidebar}>
+        <p>Sidebar content here</p>
+      </aside>
+      <main className={styles.content}>
+        <p>main content goes here </p>
+      </main>
+    </div>
+  )
 }
 
 export default Home
